9ebd6ec8c04e2bf95fa5dc4c19ed5f06b1230018
Oscar <orodriguezc1603@alumno.ipn.mx>
2023-02-18 00:33:20 -0600

    clean app

--- Backend/src/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as Products from '../controllers/products.controller.js';
-
-const router = express.Router();
-
-router.get('/all', Products.getProducts);
-router.post('/create', Products.createProduct);
-
-export default router;
